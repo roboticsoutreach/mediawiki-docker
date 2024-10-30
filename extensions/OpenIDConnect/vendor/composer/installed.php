7fdd844ab8eac904db2a28e370b6aeb891c69873
George Peppard <me@gpeppard.com>
2024-10-30 21:13:42 +0000

    add vendor

---
+++ extensions/OpenIDConnect/vendor/composer/installed.php
@@ -0,0 +1,47 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'aliases' => array(),
+        'reference' => '83024de3bdd67f0ecff6347aa97d128777d433ff',
+        'name' => 'mediawiki/openidconnect',
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'aliases' => array(),
+            'reference' => '12fb2dfe5e16183de69e784a7b84046c43d97e8e',
+        ),
+        'jumbojett/openid-connect-php' => array(
+            'pretty_version' => 'v0.9.10',
+            'version' => '0.9.10.0',
+            'aliases' => array(),
+            'reference' => '45aac47b525f0483dd4db3324bb1f1cab4666061',
+        ),
+        'mediawiki/openidconnect' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'aliases' => array(),
+            'reference' => '83024de3bdd67f0ecff6347aa97d128777d433ff',
+        ),
+        'paragonie/constant_time_encoding' => array(
+            'pretty_version' => 'v3.0.0',
+            'version' => '3.0.0.0',
+            'aliases' => array(),
+            'reference' => 'df1e7fde177501eee2037dd159cf04f5f301a512',
+        ),
+        'paragonie/random_compat' => array(
+            'pretty_version' => 'v9.99.100',
+            'version' => '9.99.100.0',
+            'aliases' => array(),
+            'reference' => '996434e5492cb4c3edcb9168db6fbb1359ef965a',
+        ),
+        'phpseclib/phpseclib' => array(
+            'pretty_version' => '3.0.41',
+            'version' => '3.0.41.0',
+            'aliases' => array(),
+            'reference' => '621c73f7dcb310b61de34d1da4c4204e8ace6ceb',
+        ),
+    ),
+);
---
+++ extensions/PluggableAuth/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'aliases' => array(),
+        'reference' => 'b35addc60fcadd6ec657d0a5df93364ef6b5b06c',
+        'name' => 'mediawiki/pluggable-auth',
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'aliases' => array(),
+            'reference' => '12fb2dfe5e16183de69e784a7b84046c43d97e8e',
+        ),
+        'mediawiki/pluggable-auth' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'aliases' => array(),
+            'reference' => 'b35addc60fcadd6ec657d0a5df93364ef6b5b06c',
+        ),
+    ),
+);
